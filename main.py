4d8a5c8cb9783b748e935dbe9e9b927e3b9b0618
jakmuell <jakob.mueller1004@gmail.com>
2024-04-20 21:20:45 +0200

    Add option is_metar

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from time import sleep
 import rp5_ru_headers
 import rp5_md_headers
 
-browsers = ['Chrome', 'Firefox', 'Opera', 'Edge']
+BROWSERS = ['Chrome', 'Firefox', 'Opera', 'Edge']
 
-def get_phpsessid(items) -> str | None:
+def get_phpsessid(items):
     phpsessid = None
     for x in items:
         if x[0] == 'PHPSESSID':
@@ -17,7 +17,8 @@ def get_phpsessid(items) -> str | None:
     return phpsessid
 
 def prepare_weatherdownload(station_id, start_date: date,
-                            last_date: date) -> None:
+                            last_date: date,
+                            is_metar: bool) -> None:
     """
     Funktioniert bisher nur für METAR Stationen, alle anderen später
     """
@@ -39,24 +40,49 @@ def prepare_weatherdownload(station_id, start_date: date,
         phpsessid = get_phpsessid(current_session.cookies.items())
 
     if url_base == 'https://rp5.ru' and phpsessid is not None:
-        current_session.headers = rp5_ru_headers.get_header(phpsessid, choice(browsers))
+        current_session.headers = rp5_ru_headers.get_header(phpsessid, choice(BROWSERS))
     elif url_base == 'https://rp5.md' and phpsessid is not None:
         current_session.headers = rp5_md_headers.get_header(phpsessid, 'Chrome')
     elif phpsessid is not None:
-        current_session.headers = rp5_ru_headers.get_header(phpsessid, choice(browsers))
+        current_session.headers = rp5_ru_headers.get_header(phpsessid, choice(BROWSERS))
     else:
         print("Error: phpsessid is None!")
     response: Response = None
     count = 5
     delay = 3
     while (response is None or response.text.find('http') == -1) and count > 0:
-        print(f"Trying post with {url_base}/responses/reFileMetar.php")
-        data={'metar': "4656", 'a_date1': start_date.strftime('%d.%m.%Y'),
-                    'a_date2': last_date.strftime('%d.%m.%Y'), 'f_ed3': 4, 'f_ed4': 4, 'f_ed5': 20, 'f_pe': 1,
-                    'f_pe1': 2, 'lng_id': 1, 'type': 'xls'}
-        print(data)
-        response: Response = current_session.post(
-            f"{url_base}/responses/reFileMetar.php", data)
+        if is_metar:
+            data={
+                'metar': station_id,
+                'a_date1': start_date.strftime('%d.%m.%Y'),
+                'a_date2': last_date.strftime('%d.%m.%Y'),
+                'f_ed3': 4,
+                'f_ed4': 4,
+                'f_ed5': 20,
+                'f_pe': 1,
+                'f_pe1': 2,
+                'lng_id': 1,
+                'type': 'csv'
+            }
+            response = current_session.post(
+                f"{url_base}/responses/reFileMetar.php", data
+            )
+        else:
+            data = {
+                'wmo_id': station_id,
+                'a_date1': start_date.strftime('%d.%m.%Y'),
+                'a_date2': last_date.strftime('%d.%m.%Y'),
+                'f_ed3': 4,
+                'f_ed4': 4,
+                'f_ed5': 20,
+                'f_pe': 1,
+                'f_pe1': 2,
+                'lng_id': 1
+            }
+            print(data)
+            response = current_session.post(
+                f"{url_base}/responses/reFileSynop.php", data
+            )
         print(response, response.text.find('http'))
         print(response.text)
         count -= 1
@@ -64,10 +90,18 @@ def prepare_weatherdownload(station_id, start_date: date,
         delay += 3
 
 if __name__ == "__main__":
+
+    # Test 1 (METAR)
     station_id = "4656"  # Sao Paulo METAR
     start_date = date(2024,2,1)
     end_date = date(2024,2,10)
-    prepare_weatherdownload(station_id, start_date, end_date)
+    prepare_weatherdownload(station_id, start_date, end_date, True)
+
+    # Test 2 (not METAR)
+    wmo_id = 83781
+    start_date = date(2024,2,1)
+    end_date = date(2024,2,10)
+    prepare_weatherdownload(wmo_id, start_date, end_date, False)
 
 
 
