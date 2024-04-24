9cede7a5e75791916fe7d17d72d80ff1a204e60c
jakmuell <jakob.mueller1004@gmail.com>
2024-04-25 00:16:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='weather_rp5',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     author='Jakob Müller',
     author_email='jakob.mueller1004@gmail.com',
